4c3784b102a468b7c46ce8ceaebb6c939ae036af
Kylektt <kylektt@outlook.com>
2024-01-11 16:04:26 +1030

    updated the home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
+import axios from 'axios';
+
+import Coins from './components/Coins';
+import Navbar from './components/Navbar';
 
 function App() {
+
+  const [coins,setCoins] = useState([]);
+
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false'
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setCoins(response.data)
+      // console.log(response.data[0])
+    }).catch((error) => {
+      console.log(error)
+    })
+  }, [])
+
   return (
     <>
-      
+  
+    <Navbar />
+    <Coins coins={coins} />
+
     </>
   );
 }
 
 export default App;
+
+
+//   useEffect(() => {
+//     axios
+//     .get(url)
+//   .then(res => {
+//    setCoins(res.data)
+//   //  console.log(res.data);
+//   }).catch(error => console.log(error))
+// },  []);
